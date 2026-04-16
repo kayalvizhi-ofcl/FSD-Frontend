cd9ecf396170de329114560e41b2565ffe45591d
kayalvizhi-ofcl <kayalvizhiofcl@gmail.com>
2026-04-16 22:25:03 +0530

    Add files via upload

---
+++ Day26 - Task/array using spread operator.js
@@ -0,0 +1,6 @@
+const originalArray = [1, 2, 3];
+
+// Copy array
+const copiedArray = [...originalArray];
+
+console.log(copiedArray); 
---
+++ Day26 - Task/destruction.js
@@ -0,0 +1,24 @@
+//Array destructing
+const numbers = [10, 20, 30];
+
+// Extract values
+const [a, b, c] = numbers;
+
+console.log(a); 
+console.log(b); 
+console.log(c); 
+
+//Object destructing
+const person = {
+  name: "Kayal",
+  age: 21,
+  city: "Chennai"
+};
+
+// Extract values
+const { name, age, city } = person;
+
+console.log(name); 
+console.log(age);  
+console.log(city); 
+
