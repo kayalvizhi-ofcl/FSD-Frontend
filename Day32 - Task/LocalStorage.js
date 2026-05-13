fc34941d05627f9c9f3b2f6a46f58e18eededd0c
kayalvizhi-ofcl <kayalvizhiofcl@gmail.com>
2026-05-13 23:35:30 +0530

    Add files via upload

---
+++ Day32 - Task/LocalStorage.js
@@ -0,0 +1,5 @@
+localStorage.setItem("username", "Kayal");
+
+console.log(localStorage.getItem("username"));
+
+localStorage.removeItem("username");
---
+++ Day32 - Task/sessionStorage.js
@@ -0,0 +1,5 @@
+sessionStorage.setItem("city", "Chennai");
+
+console.log(sessionStorage.getItem("city"));
+
+sessionStorage.removeItem("city");
